23e546241a9f9c2d60fa7755bd1de1240c8b7d15
riskmaris <mariskaps20@gmail.com>
2023-08-02 20:01:18 +0300

    Modify Apicomment.js to run tests

--- src/Apicomment.js
+++ src/Apicomment.js
@@ -1,35 +1,69 @@
-export default class commentApi {
-    static commentId = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/jcfL4bzfqSk0ihg1PeIe/comments';
+// export default class commentApi {
+//     static commentId = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/jcfL4bzfqSk0ihg1PeIe/comments';
 
-    static getComments = async (id) => {
-      const response = await fetch(`${this.commentId}?item_id=${id}`);
+//     static getComments = async (id) => {
+//       const response = await fetch(`${this.commentId}?item_id=${id}`);
 
-      const data = await response.json();
-      return data;
-    };
+//       const data = await response.json();
+//       return data;
+//     };
 
-    static setComments = async (id, username, comment) => {
-      const response = await fetch(this.commentId, {
-        method: 'post',
-        body: JSON.stringify({
-          item_id: id,
-          username,
-          comment,
-        }),
-        headers: {
-          'Content-type': 'application/JSON',
-        },
-      });
+//     static setComments = async (id, username, comment) => {
+//       const response = await fetch(this.commentId, {
+//         method: 'post',
+//         body: JSON.stringify({
+//           item_id: id,
+//           username,
+//           comment,
+//         }),
+//         headers: {
+//           'Content-type': 'application/JSON',
+//         },
+//       });
 
-      const data = await response.text();
-      return data;
-    };
+//       const data = await response.text();
+//       return data;
+//     };
 
-      static counterComments = (data) => {
-        let count = 0;
-        for (let i = 0; i < data.length; i += 1) {
-          count += 1;
-        }
-        return count;
-      };
+//       static counterComments = (data) => {
+//         let count = 0;
+//         for (let i = 0; i < data.length; i += 1) {
+//           count += 1;
+//         }
+//         return count;
+//       };
+// }
+
+class CommentApi {
+  static commentId =
+    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/jcfL4bzfqSk0ihg1PeIe/comments';
+
+  static getComments = async (id) => {
+    const response = await fetch(`${this.commentId}?item_id=${id}`);
+    const data = await response.json();
+    return data;
+  };
+
+  static setComments = async (id, username, comment) => {
+    const response = await fetch(this.commentId, {
+      method: 'post',
+      body: JSON.stringify({
+        item_id: id,
+        username,
+        comment,
+      }),
+      headers: {
+        'Content-type': 'application/JSON',
+      },
+    });
+
+    const data = await response.text();
+    return data;
+  };
+
+  static counterComments = (data) => {
+    return data.length; // Use array's length property to get the count
+  };
 }
+
+module.exports = CommentApi;
