e99ca917ef7d5a4f3d7e6cee61e62557727c6917
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-08-03 19:05:30 +0100

    Write test for movie counter

--- module/counter.js
+++ module/counter.js
@@ -2,6 +2,7 @@ const countMovies = () => {
   const moviesLink = document.querySelector('#main-title');
   const moviesCount = document.querySelectorAll('.each-movie').length;
   moviesLink.textContent = `Top Movies (${moviesCount})`;
+  return moviesCount;
 };
 
 export default countMovies;
